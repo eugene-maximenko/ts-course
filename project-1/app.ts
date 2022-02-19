b9f90ade39a9a21c7d2f02490e5447fcbecc8b42
ShadowPrince <zhek4770@gmail.com>
2022-02-19 22:10:35 +0200

    Train literal types

--- project-1/app.ts
+++ project-1/app.ts
@@ -1,9 +1,27 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
+function combine(
+    input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text'
+) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString()
+    }
+
+    return result
+
+    // if (resultConversion === 'as-number') {
+    //     return +result
+    // } else {
+    //     return result.toString()
+    // }
 }
 
-const number1 = 5
-const number2 = 2.8
+const combinedAges = combine(30, 26, 'as-number')
+console.log(combinedAges)
 
-const result = add(number1, number2)
-console.log(result)
+const combinedStringAges = combine('30', '26', 'as-number')
+console.log(combinedStringAges)
+
+const combinedNames = combine('Max', 'Anna', 'as-text')
+console.log(combinedNames)
