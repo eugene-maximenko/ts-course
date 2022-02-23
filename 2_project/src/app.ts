028e7c9bf6d7c65e7fb8a70263ababd250da0492
ShadowPrince <zhek4770@gmail.com>
2022-02-23 18:45:44 +0200

    Finished configure tscongif

--- 2_project/src/app.ts
+++ 2_project/src/app.ts
@@ -1,5 +1,18 @@
-const button1 = document.querySelector('button')!
+let appId = 'abc'
+const button = document.querySelector('button')
 
-button1.addEventListener('click', () => {
-    console.log('Clicked!')
-})
+function add(n1: number, n2: number) {
+    if (n1 + n2 > 0) {
+        return n1 + n2
+    }
+    return
+}
+
+function clickHandler(message: string) {
+    // let userName = 'Max'
+    console.log('Clicked!' + message);
+}
+
+if (button) {
+    button.addEventListener('click', clickHandler.bind(null, 'You`re welcome'))
+}
