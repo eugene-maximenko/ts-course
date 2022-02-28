32672a1348a44c5635a79e3f1d16f80e3a819fd4
ShadowPrince <zhek4770@gmail.com>
2022-02-28 20:49:07 +0200

    Init 3_project

---
+++ 3_project/src/app.ts
@@ -0,0 +1,25 @@
+const userName = 'Max'
+// userName = 'Maximillian'
+let age = 30
+
+age = 29
+var result
+
+// function add(a: number, b: number) {
+//     result = a + b
+//     return result
+// }
+
+// console.log(result);
+
+const add = (a: number, b: number) => a + b
+
+const printOutput: (a: number | string) => void = output => console.log(output)
+
+const button = document.querySelector('button')
+
+if (button) {
+    button.addEventListener('click', event => console.log(event))
+}
+
+printOutput(add(5, 2))
