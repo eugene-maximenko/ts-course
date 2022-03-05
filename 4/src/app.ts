39b81da7ead5fe52d284e419167092e61e44e2af
ShadowPrince <zhek4770@gmail.com>
2022-03-05 16:43:01 +0200

    Explore inheritance and accessors

--- 4/src/app.ts
+++ 4/src/app.ts
@@ -1,9 +1,16 @@
 class Department {
-    // name: string
-    private employees: string[] = []
+    static fiscalYear = 2020
 
-    constructor (private id: string, public name: string) {
+    // name: string
+    protected employees: string[] = []
+
+    constructor(private readonly id: string, public name: string) {
         // this.name = n
+        console.log(Department.fiscalYear);
+    }
+
+    static createEmployee(name: string) {
+        return { name }
     }
 
     describe(this: Department) {
@@ -20,13 +27,76 @@ class Department {
     }
 }
 
-const accounting = new Department('Accounting', 'Leila')
+class ITDepartment extends Department {
+    constructor(id: string, public admins: string[]) {
+        super(id, 'IT')
+    }
+}
+
+const employee1 = Department.createEmployee('Max')
+
+console.log(employee1, Department.fiscalYear);
+
+const it = new ITDepartment('Accounting', ['Max'])
+
+it.addEmployee('Max')
+it.addEmployee('Manu')
+// accounting.employees[2] = 'Anna' 
+
+it.describe()
+it.printEmployeeInformation()
+
+class AccountingDepartment extends Department {
+    private lastReport: string
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport
+        }
+        throw new Error('No report found.')
+    }
+
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!')
+        }
+        this.addReport(value)
+    }
+
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting')
+        this.lastReport = reports[0]
+    }
+
+    addEmployee(name: string) {
+        if (name === 'Max') {
+            return
+        }
+        this.employees.push(name)
+    }
+
+    addReport(text: string) {
+        this.reports.push(text)
+        this.lastReport = text
+    }
+
+    printReports() {
+        console.log(this.reports)
+    }
+}
+
+console.log(it)
+
+const accounting = new AccountingDepartment('d2', [])
+
+accounting.mostRecentReport = 'Year End Report'
+accounting.addReport('Something went wrong...')
+console.log(accounting.mostRecentReport)
 
 accounting.addEmployee('Max')
 accounting.addEmployee('Manu')
-// accounting.employees[2] = 'Anna'
 
-accounting.describe()
+accounting.printReports()
 accounting.printEmployeeInformation()
 
 // const accountingCopy = {
