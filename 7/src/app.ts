822e6d74eb460ef66c53169a648646dc342b2f70
ShadowPrince <zhek4770@gmail.com>
2022-04-07 23:17:19 +0300

    Build one more decorator

--- 7/src/app.ts
+++ 7/src/app.ts
@@ -5,7 +5,20 @@ function Logger(logString: string) {
   };
 }
 
-@Logger('LOGGING - PERSON ')
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const person = new constructor();
+
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = person.name;
+    }
+  };
+}
+
+// @Logger('LOGGING - PERSON ')
+@WithTemplate('<h1>My Person Object</h2>', 'app')
 class Person {
   name = 'Max';
 
