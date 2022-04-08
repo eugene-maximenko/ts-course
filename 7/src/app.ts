a76e2d217c5424b7218070122bb917bdf5e7b030
ShadowPrince <zhek4770@gmail.com>
2022-04-08 23:56:55 +0300

    Explore property decorator

--- 7/src/app.ts
+++ 7/src/app.ts
@@ -37,3 +37,34 @@ class Person {
 const person = new Person();
 
 console.log(person);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property decorator!');
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  @Log
+  private _price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+// new Product('4', 4);
