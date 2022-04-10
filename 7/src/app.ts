919405dde49613819fc1e0d8857bab5df2911e7f
ShadowPrince <zhek4770@gmail.com>
2022-04-10 21:40:17 +0300

    Add decorator to method

--- 7/src/app.ts
+++ 7/src/app.ts
@@ -104,11 +104,23 @@
 // const p1 = new Product('Book', 19);
 // const p2 = new Product('Book 2', 29);
 
-function AutoBind() {}
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 
 class Printer {
   message = 'This works!';
 
+  @AutoBind
   showMessage() {
     console.log(this.message);
   }
