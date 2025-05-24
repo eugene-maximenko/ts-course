08969c222b27134e9818a4985b8698c4580c724f
Eugene Maximenko <zhek4770@gmail.com>
2025-05-24 18:44:32 +0200

    Check functions

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
-let sales = 123456;
-let course = 'TypeScript';
-let is_published = true;
-let level = true;
-level = 25;
-level = 'asdf';
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
