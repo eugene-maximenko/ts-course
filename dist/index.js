6445067c49674ef6e0918db0c62e69d03860b661
Eugene Maximenko <zhek4770@gmail.com>
2025-05-27 15:48:40 +0200

    Exploring generic interfaces

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,9 @@
 "use strict";
-class Person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-    get fullName() { return this.firstName + ' ' + this.lastName; }
-    walk() {
-        console.log('Walking');
+class KeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
     }
 }
-class Student extends Person {
-    constructor(studentId, firstName, lastName) {
-        super(firstName, lastName);
-        this.studentId = studentId;
-    }
-    takeTest() {
-        console.log('Taking a test');
-    }
-}
-class Teacher extends Person {
-    get fullName() { return 'Professor' + " " + super.fullName; }
-}
-class Principal extends Person {
-    get fullName() {
-        return 'Principal' + super.fullName;
-    }
-}
-printNames([
-    new Student(1, 'John', 'Smith'),
-    new Teacher('Mosh', 'Hamedani'),
-    new Principal('Mary', 'Smith')
-]);
-function printNames(people) {
-    for (let person of people) {
-        console.log(person.fullName);
-    }
-}
+let pair = new KeyValuePair('1', 'Apple');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,15 @@
-class Person {
-
-    constructor(public firstName: string, public lastName: string) { }
-
-    get fullName() { return this.firstName + ' ' + this.lastName }
-
-    walk() {
-        console.log('Walking');
-    }
+interface Result<T> {
+    data: T | null,
+    error: string | null
 }
 
-class Student extends Person {
-
-    constructor(public studentId: number, firstName: string, lastName: string) {
-        super(firstName, lastName)
-    }
-
-    takeTest() {
-        console.log('Taking a test');
-
-
-    }
+function fetch<T>(url: string): Result<T> {
+    return { data: null, error: null }
 }
 
-class Teacher extends Person {
+interface User { username: string }
 
-    override get fullName() { return 'Professor' + " " + super.fullName }
+interface Product { title: string }
 
-}
-
-class Principal extends Person {
-    override get fullName() {
-        return 'Principal' + super.fullName
-    }
-}
-
-printNames([
-    new Student(1, 'John', 'Smith'),
-    new Teacher('Mosh', 'Hamedani'),
-    new Principal('Mary', 'Smith')
-])
-
-function printNames(people: Person[]) {
-    for (let person of people) {
-        console.log(person.fullName);
-    }
-}
+let result = fetch<Product>('url')
+result.data?.title
